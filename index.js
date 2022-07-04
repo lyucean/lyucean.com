e17c7d31f230a049fb974453b8bc720ef6cef339
lyucean <2kygc6sqQzajmR>
2022-07-04 12:07:17 +0300

    +timer

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-document.addEventListener("DOMContentLoaded", () =>
-  requestAnimationFrame(updateTime)
-);
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
-function updateTime() {
-  document.documentElement.style.setProperty(
-    "--timer-day",
-    "'" + moment().format("dd") + "'"
-  );
-  document.documentElement.style.setProperty(
-    "--timer-hours",
-    "'" + moment().format("k") + "'"
-  );
-  document.documentElement.style.setProperty(
-    "--timer-minutes",
-    "'" + moment().format("mm") + "'"
-  );
-  document.documentElement.style.setProperty(
-    "--timer-seconds",
-    "'" + moment().format("ss") + "'"
-  );
-  requestAnimationFrame(updateTime);
-}
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy + 1,
+    dayMonth = "07/15/",
+    birthday = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+
+  const countDown = new Date(birthday).getTime(),
+    x = setInterval(function() {
+
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "It's my birthday!";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(x);
+      }
+      //seconds
+    }, 0)
+}());
