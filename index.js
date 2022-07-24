c5cd149836cf7febef0c7e497ac4546bfdcd6529
lyucean@gmail.com <Nosferatu2018>
2022-07-24 21:08:38 +0300

    Задержка побольше

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var offcanvas = new bootstrap.Offcanvas(offcanvas_el)
 var nav_links = offcanvas_el.querySelectorAll('.nav-link')
 
 for (var i = 0; i < nav_links.length; i++) {
+
   nav_links[i].onclick = function () {
     offcanvas.hide()
     setTimeout(function () {
       window.location = window.location.href
-    }, 100)
+    }, 300)
   }
 }
 
