ecfbfe8f997aea6a11e24f638ee97cadeda9ead4
lyucean@gmail.com <Nosferatu2018>
2022-07-25 00:48:10 +0300

    В bootstrap скрытые якоря работают только так :D

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var offcanvas_el = document.getElementById('leftMobileMenu')
-var offcanvas = new bootstrap.Offcanvas(offcanvas_el)
-var nav_links = offcanvas_el.querySelectorAll('.nav-link')
-
-for (var i = 0; i < nav_links.length; i++) {
-
-  nav_links[i].onclick = function () {
-    offcanvas.hide()
-    setTimeout(function () {
-      window.location = window.location.href
-    }, 300)
-  }
-}
+// var offcanvas_el = document.getElementById('leftMobileMenu')
+// var offcanvas = new bootstrap.Offcanvas(offcanvas_el)
+// var nav_links = offcanvas_el.querySelectorAll('.nav-link')
+//
+// for (var i = 0; i < nav_links.length; i++) {
+//
+//   nav_links[i].onclick = function () {
+//     offcanvas.hide()
+//     setTimeout(function () {
+//       window.location = window.location.href
+//     }, 300)
+//   }
+// }
 
