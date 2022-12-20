b578403046a0389e543055fe97ece5c26f65bb38
lyucean@gmail.com <NXmB7i47VFmni>
2022-12-21 00:09:34 +0300

    Поправил прокрутку до нужного элемента

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-// leftMobileMenu
-var menuLeftMobile = document.getElementById('menuLeftMobile')
-var offcanvas = new bootstrap.Offcanvas(menuLeftMobile)
-var nav_links = menuLeftMobile.querySelectorAll('.nav-link')
+const menuLeftMobile = document.getElementById('menuLeftMobile')
+const offcanvas = new bootstrap.Offcanvas(menuLeftMobile)
+const nav_links = menuLeftMobile.querySelectorAll('.nav-link')
+const menuTop = document.getElementById('menuTop')
 
-for (var i = 0; i < nav_links.length; i++) {
-  nav_links[i].onclick = function () {
-    offcanvas.hide()
-
-    setTimeout(function () {
-      window.location = window.location.href
-    }, 300)
+// Прокрутка до нужного элемента страница
+function scrollToElement (id) {
+  if (window.screen.width < 768) {
+    window.scrollTo({
+      top: document.getElementById(id).offsetTop - menuTop.offsetHeight,
+      behavior: 'smooth'
+    })
+  } else {
+    window.scrollTo({
+      top: document.getElementById(id).offsetTop - 10,
+      behavior: 'smooth'
+    })
   }
 }
 
-var width = window.screen.width
-var menuTop = document.getElementById('menuTop')
-if (width < 768) {
-  menuTop.classList.add('sticky-top')
-  menuLeftMobile.classList.add('text-bg-dark2')
-} else {
-  menuTop.classList.remove('sticky-top')
-  menuLeftMobile.classList.remove('text-bg-dark2')
+// изменим действия кнопок меню по умолчанию
+for (let i = 0; i < nav_links.length; i++) {
+  nav_links[i].onclick = function () {
+    offcanvas.hide()
+    scrollToElement(this.hash.slice(1))
+    return false
+  }
 }
+
+// Слушаем изменение экрана 
+window.addEventListener('resize', resizeDom)
+
+function resizeDom () {
+  if (window.screen.width < 768) {
+    menuTop.classList.add('sticky-top') // закрепляем меню в Top
+    menuLeftMobile.classList.add('text-bg-dark2') // Меням цвет у бокового меню
+  } else {
+    menuTop.classList.remove('sticky-top')
+    menuLeftMobile.classList.remove('text-bg-dark2')
+  }
+}
+
