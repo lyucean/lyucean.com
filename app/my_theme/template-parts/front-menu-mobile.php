f21f0c61dee46d241f0b235188d0d228acde16f8
lyucean <lyucean@gmail.com>
2024-11-20 14:25:28 +0300

    Поправил отображение в мобильном меню

--- app/my_theme/template-parts/front-menu-mobile.php
+++ app/my_theme/template-parts/front-menu-mobile.php
@@ -2,7 +2,7 @@
 // Проверяем, зарегистрировано ли меню
 if (has_nav_menu('sidebar-menu')) : ?>
     <!-- Мобильное меню -->
-    <div class="front-menu offcanvas-md offcanvas-start d-md-none" tabindex="-1" id="mobileSidebarMenu" aria-labelledby="mobileSidebarMenuLabel" style="width: 280px;">
+    <div class="front-menu-mobile offcanvas-md offcanvas-start d-md-none" tabindex="-1" id="mobileSidebarMenu" aria-labelledby="mobileSidebarMenuLabel" style="width: 280px;">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="mobileSidebarMenuLabel">Меню</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#mobileSidebarMenu" aria-label="Close"></button>
