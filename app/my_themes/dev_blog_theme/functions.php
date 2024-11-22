5d186b9281c537b0de5c51a0f8f9ce6f28a08eca
lyucean <lyucean@gmail.com>
2024-11-22 17:59:20 +0300

    Перенесем папку в свою тему и упростим добавление других тем в будущем

--- app/my_themes/dev_blog_theme/functions.php
+++ app/my_themes/dev_blog_theme/functions.php
@@ -2,7 +2,7 @@
 // Используется для добавления функций и подключения стилей/скриптов.
 
 // Подключаем стили и скрипты
-function my_theme_enqueue_styles() {
+function dev_blog_theme_enqueue_styles() {
     // Подключаем Bootstrap
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
     // Bootstrap Icons
@@ -11,9 +11,9 @@ function my_theme_enqueue_styles() {
     wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array(), null, true);
 
     // Подключаем стили темы
-    wp_enqueue_style('my_theme-style', get_stylesheet_uri());
+    wp_enqueue_style('dev_blog_theme-style', get_stylesheet_uri());
 }
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles'); // Подключаем стили и скрипты когда WordPress загружает скрипты и стили
+add_action('wp_enqueue_scripts', 'dev_blog_theme_enqueue_styles'); // Подключаем стили и скрипты когда WordPress загружает скрипты и стили
 
 // Поддержка миниатюр. Нужен т.к. я планирую использовать миниатюры для постов/страниц
 add_theme_support('post-thumbnails');
@@ -56,7 +56,7 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
 // Регистрация меню. Нужно только если у вас будет навигационное меню, которое можно редактировать через админку
 function register_theme_menus() {
     register_nav_menus(array(
-        'sidebar-menu' => __('Боковое меню', 'my_theme'),
+        'sidebar-menu' => __('Боковое меню', 'dev_blog_theme'),
     ));
 }
 add_action('init', 'register_theme_menus');
@@ -146,7 +146,7 @@ function set_post_views() {
 add_action('wp_head', 'set_post_views');
 
 
-// Функция для получения случайного SVG-паттерна (добавить в functions.php)
+// Функция для получения SVG-паттерна
 function get_random_pattern() {
     return '<svg width="100%" height="100%" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <defs>
